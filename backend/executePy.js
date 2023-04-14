1195448b011a5178e473532732a3892eb60f8dc2
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2023-04-14 13:27:43 +0530

    Python language added

---
+++ backend/executePy.js
@@ -0,0 +1,21 @@
+const {exec} = require("child_process")
+const { stdout, stderr } = require("process")
+
+// make an execuatable file
+const executePy = (filepath) =>{
+    return new Promise((resolve, reject)=>{
+        exec(`python ${filepath}`, (error, stdout, stderr)=>{
+            if (error){
+                reject({error, stderr});
+            }
+            if(stderr){
+                reject(stderr);
+            }
+            resolve(stdout);
+        })
+    })
+}
+
+module.exports = {
+    executePy
+}
--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,45 @@
-const express = require ("express");
-const cors = require ("cors")
-const {generateFile} = require("./generateFile")
-const {executeCpp} = require("./executeCpp")
+const express = require("express");
+const cors = require("cors");
+const { generateFile } = require("./generateFile");
+const { executeCpp } = require("./executeCpp");
+const { executePy } = require("./executePy");
 
 const app = express();
 
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  return res.json({ hello: "world!" });
+});
 
-app.get("/", ( req, res )=>{
-return res.json({hello: "world!"})
-})
+app.post("/run", async (req, res) => {
+  const { language = "cpp", code } = req.body;
+  console.log(language, code.length)
+  if (code === undefined)
+    res
+      .status(400)
+      .json({ success: false, error: "The code body cannot be empty" }); // bad request if there is no code
 
-app.post("/run", async (req, res)=>{
-    const {language = "cpp", code} = req.body;
-    if(code === undefined) res.status(400).json({success: false, error: "The code body cannot be empty"}); // bad request if there is no code
+  try {
+    // we need to generate a cpp file with content from request
+    const filepath = await generateFile(language, code);
+    // we need to run the file and then send the response
 
-    try {
-      // we need to generate a cpp file with content from request
-      const filepath = await generateFile(language, code);
-      // we need to run the file and then send the response
-      const output = await executeCpp(filepath);
-
-      return res.json({ filepath, output });
-    } 
-    catch (error) {
-        res.status(500).json({error});
+    let output;
+    if (language === "cpp") {
+      output = await executeCpp(filepath);
+    } else {
+      output = await executePy(filepath);
     }
-    
-})
 
+    return res.json({ filepath, output });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
-app.listen(5000, ()=>{
-    console.log("Listening on port 5000");
-})
+app.listen(5000, () => {
+  console.log("Listening on port 5000");
+});
