f95e1712beca5281ae51b45dceb32082095d3b56
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2023-04-21 03:29:15 +0530

    Lottie animation added

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-// import icon from "../public/favicon.ico"
+import Logo from "../public/LiveCodeX logo red.json";
+import Lottie from "lottie-react";
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,12 +19,34 @@ export default function Home() {
           content="Online code compiler that can be used by interviewers to evaluate the interviewee's performance. It shows code written by interviewee to the interviewer in real time. Interviewer simply can make a room and send the code to the interviewee so they can join the room and write thier codes and compile them"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-<link rel="manifest" href="/site.webmanifest"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
-      <main>Home page</main>
+      <main>
+        <>
+          <h1>Home page</h1>
+          <div className='w-10'>
+          <Lottie loop={true} animationData={Logo} height={100} width={100} />
+          </div>
+          <h1>workign</h1>
+        </>
+      </main>
     </>
   );
 }
